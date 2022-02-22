b4fed9b8d30003f282355700f6def574ad575f09
Mateusz Hadryś <hadrys.mateusz86@gmail.com>
2022-02-22 09:02:32 +0100

    attempt to use yet another path

--- app/post.ts
+++ app/post.ts
@@ -6,12 +6,12 @@ import { marked } from "marked"
 import hljs from "highlight.js"
 
 // TODO: this is an attempt to temporarily force these files to be included in build, but a permanent solution is required, probably by using mdx routes like for projects
-import "../posts/background-properties-explained.md"
-import "../posts/css-gradients-guide.md"
-import "../posts/css-list-styling.md"
-import "../posts/font-shorthand.md"
-import "../posts/js-get-all-characters-in-string.md"
-import "../posts/gradient-text.md"
+import "./posts/background-properties-explained.md"
+import "./posts/css-gradients-guide.md"
+import "./posts/css-list-styling.md"
+import "./posts/font-shorthand.md"
+import "./posts/js-get-all-characters-in-string.md"
+import "./posts/gradient-text.md"
 
 export type Post = {
   slug: string
