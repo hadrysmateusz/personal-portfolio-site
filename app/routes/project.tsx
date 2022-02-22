6b635da7cdae7cb6a14abf9a5d8f5445b31260cf
Mateusz Hadryś <hadrys.mateusz86@gmail.com>
2022-02-22 07:41:31 +0100

    add meta export to project index

--- app/routes/project.tsx
+++ app/routes/project.tsx
@@ -1,7 +1,11 @@
-import { Outlet } from "remix"
+import { MetaFunction, Outlet } from "remix"
 
 import { PageContainer, PageHeader } from "~/components"
 import { Footer } from "~/components/Footer/Footer"
+
+export const meta: MetaFunction = () => {
+  return { title: "Mateusz Hadryś - Project Case Study" }
+}
 
 export default function ProjectIndex() {
   return (
